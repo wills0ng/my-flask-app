efb2bcce81317030431f0ee3c93ae6f480479f31
Will Song <willsong@Wills-MacBook-Pro.local>
2023-04-14 11:05:36 -0400

    added form.html and subscribe.html, updated app.py to allow user to input info to subscribe to form

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+subscribers = []
+
 @app.route('/')
 def index():
-    title = "Will Song's Portfolio"
-    return render_template("index.html", title=title)
+    return render_template("index.html")
 
 @app.route('/about')
 def about():
-    title = "About Will Song"
-    return render_template("about.html", title=title)
+    return render_template("about.html")
+
+@app.route('/subscribe')
+def subscribe():
+    return render_template("subscribe.html")
+
+@app.route('/form', methods=["POST"])
+def form():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+    subscribers.append(f"{first_name} {last_name} | {email}")
+    return render_template("form.html", first_name=first_name, last_name=last_name, email=email, subscribers=subscribers)
