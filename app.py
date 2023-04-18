05c43c79aeab2c80fa6b17480f840ca9500f185c
Will Song <willsong@Wills-MacBook-Pro.local>
2023-04-18 12:35:46 -0400

    added logic for all form fields being required for subscribe.html in app.py and modified fail.html

--- app.py
+++ app.py
@@ -21,5 +21,13 @@ def form():
     first_name = request.form.get("first_name")
     last_name = request.form.get("last_name")
     email = request.form.get("email")
+
+    if not first_name or not last_name or not email:
+        error_statement = "All Form Fields Required..."
+        return render_template("subscribe.html", error_statement=error_statement,
+                               first_name=first_name,
+                               last_name=last_name,
+                               email=email)
+
     subscribers.append(f"{first_name} {last_name} | {email}")
     return render_template("form.html", first_name=first_name, last_name=last_name, email=email, subscribers=subscribers)
